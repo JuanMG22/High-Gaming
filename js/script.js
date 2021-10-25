e7b91680a04a3c1933d9890861dc29b743a35cb7
Juan Manuel González <juan.m-gonzalez@outlook.com>
2021-10-24 23:03:41 -0300

    DOM - Interactuar con HTML
    Ahora se muestra el carrito de compras en una lista ordenada

--- js/script.js
+++ js/script.js
@@ -1,34 +1,80 @@
-const carritoDeCompras = []; //Creo un array vacío, donde despues agrego los productos
-const precios = []; //Creo un array vacio donde se ingresarán precios de los productos
+let carritoDeCompras = []; //Creo un array vacío, donde despues agrego los productos
+let precios = []; //Creo un array vacio donde se ingresarán precios de los productos
 const precioTotal = (previousValue, currentValue) => previousValue + currentValue;
 
+// Botones
+const botonComprar1 = document.querySelector(`#btnComprar1`);
+const botonComprar2 = document.querySelector(`#btnComprar2`);
+const botonComprar3 = document.querySelector(`#btnComprar3`);
+const botonComprar4 = document.querySelector(`#btnComprar4`);
+
+const botonMostrar = document.querySelector(`#btnMostrar`);
+const botonLimpiar = document.querySelector(`#btnLimpiar`);
+
+// Lista de productos
+const listaTitulo = document.querySelector(`#carritoTitulo`);
+const listaProductos = document.querySelector(`#carritoProductos`);
+let li;
+const listaTotal = document.querySelector(`#carritoTotal`);
+
+// Eventos
+botonComprar1.addEventListener("click", () => {
+    agregarCpu(`AMD Ryzen 5 2600`, 27030);
+});
+
+botonComprar2.addEventListener("click", () => {
+    agregarCpu(`AMD Ryzen 5 3500X`, 28930);
+});
+
+botonComprar3.addEventListener("click", () => {
+    agregarCpu(`AMD Ryzen 5 5950X`, 90330);
+});
+
+botonComprar4.addEventListener("click", () => {
+    agregarCpu(`AMD Ryzen 5 3950X`, 115730);
+});
+
+botonMostrar.addEventListener("click", () => {
+    mostrarCarrito();
+});
+
+botonLimpiar.addEventListener("click", () => {
+    limpiarCarrito();
+});
+
+
 // Funcion para agregar productos
-function agregarCpu(nombre, precio) {
+const agregarCpu = (nombre, precio) => {
     carritoDeCompras.push(nombre);
     alert(`Se agregó el producto`);
     return precios.push(precio);
 }
 
 
-// Función que muestracon un alert el precio total 
-function mostrarCarrito() {
-    for (let index = 0; index < carritoDeCompras.length; index++) {
-        iCarrito = carritoDeCompras[index];
-        console.log(`${iCarrito}`)
+// Función que muestra carrito con precio total y productos ingresados en un li
+const mostrarCarrito = () => {
+    for (const producto of carritoDeCompras) {
+        li = document.createElement("li")
+        li.innerHTML = producto;
+        listaProductos.appendChild(li);
     }
-
-    if (precios.length > 1) {
-        alert(`El precio total de la compra es de $ ${precios.reduce(precioTotal)}\nLista de productos en consola`);
+    if (precios.length > 0) {
+        listaTitulo.textContent = (`Carrito de Compras`);
+        listaTotal.textContent = (`El precio total de la compra es de $ ${precios.reduce(precioTotal)}`);
         return precioTotal;
     } else {
         alert("No se ingresó ningún producto")
     }
 }
 
-// Funcio que vacia el carrito por medio de un splice
-function limpiarCarrito() {
-    largoPrecios = precios.length; //Hallo el largo del array precios
-    console.clear();
+// Funcion que vacia el carrito
+const limpiarCarrito = () => {
+    precios = [];
+    carritoDeCompras = [];
+    listaTitulo.textContent = (``);
+    listaTotal.textContent = (``);
+    listaProductos.parentNode.removeChild(listaProductos);
+    location.reload();
     alert(`Se vacio el carrito`);
-    return precios.splice(0, largoPrecios);
+
 }
