fa1e41c07ccf33d5a560db3b843fc824d8e6e711
Juan Manuel González <juan.m-gonzalez@outlook.com>
2021-11-14 19:22:49 -0300

    Ajax
    Se agregó ajax al proyecto, se agregó un boton en la seccion productos que muestra una lista de productos, trayendo daots de un archivo json

--- js/ajax.js
+++ js/ajax.js
@@ -1,18 +1,32 @@
-const URLJSON = 'productos.json';
-
-$('#btn-ajax').click( () => {
-    $.getJSON( URLJSON, (response, status) => {
-
-        if (status !== 'success') {
-            throw new Error('error')
+//Declaramos la url que vamos a usar para el GET
+const URLJSON = "../productos.json"
+//Agregamos un botón con jQuery
+$("#lista-productos").append('<button id="btn1" class="btn btn-dark my-5">Mostrar Lista</button>');
+//Escuchamos el evento click del botón agregado
+$("#btn1").click(() => {
+    $.get(URLJSON, function (respuesta, estado) {
+        if (estado === "success") {
+            let misDatos = respuesta;
+            for (const dato of misDatos) {
+                $("#lista-productos").append(`
+                <table class="table table-dark table-striped table-hover">
+                    <thead>
+                        <tr>
+                        <th scope="col m-0">#</th>
+                        <th scope="col">Producto</th>
+                        <th scope="col">Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">${dato.id}</th>
+                        <td><img style="width: 5%" src=".${dato.imagen}"> ${dato.titulo}</td>
+                        <td>$ ${dato.precio}</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                `);
+            }
         }
-
-        for (const article of response) {
-            $('#content').prepend(`
-
-                <h3> ${article.titulo} </h3>
-
-            `)
-        }
-    })
-})
+    });
+});
